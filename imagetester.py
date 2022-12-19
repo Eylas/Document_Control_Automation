e43b81f005a131ceae979d60371f23dd39b177bf
Nathan <nathan.orr@nodehex.com>
2022-12-19 17:02:06 +0100

    pdf_crop_selector added
    - Added functionality for user input and crop selection fot future use.

--- imagetester.py
+++ imagetester.py
@@ -1,13 +1,12 @@
 import pdfplumber
 from wand.image import Image
 from wand.display import display
+import re
+import os
 
 def image_print(name, img):
     img = img.to_image(resolution=150)
     img.save(f"img_page_number_{img.page.page_number}_{name}.png", format="PNG")
-
-
-test_var = "bob"
 
 with pdfplumber.open(r'C:\Users\Natha\Document_Control_Automation\loading_area\C321-MMD-RT-DPP-130-591702.pdf') as pdf:
     for page in pdf.pages:
@@ -30,5 +29,7 @@ with pdfplumber.open(r'C:\Users\Natha\Document_Control_Automation\loading_area\C
         for crop, img in crops.items():
             image_print(crop, img)
         
-  
+dwg_pattern = re.compile(r'\w\d\d\d.\w\w\w.\w\w.\w\w\w.\d\d\d.\d\d\d\d\d\d')
+rev_pattern = re.compile(r'P\d\d') 
 
+path = r'C:\Users\Natha\Document_Control_Automation\loading_area'
